430181c505d31ffcaa07a66074331f17f8ced54a
yeka <rongjun0821@gmail.com>
2024-04-22 14:50:42 +0800

    改进镜像构建方式，适配Pod启动下配置文件的初始化逻辑

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
+# 第一阶段：构建 Go 应用程序
+FROM golang:1.14 AS builder
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -o app .
+
+# 第二阶段：创建运行镜像
 FROM alpine:3.12
-COPY ./app /app
-ENTRYPOINT /app
+WORKDIR /root/
+COPY --from=builder /app/app .
+CMD ["./app"]
--- collector/collector.go
+++ collector/collector.go
@@ -7,6 +7,7 @@ import (
 	coreV1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"net/http"
 	"os"
@@ -35,23 +36,32 @@ func homeDir() string {
 }
 
 func NewMetrics() *Metrics {
-	// creates the in-cluster config
-	//config, err := rest.InClusterConfig()
-
-	var kubeconfig *string
-	if home := homeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	var config *rest.Config
+	var err error
+	if os.Getenv("KUBERNETES_SERVICE_HOST") != "" && os.Getenv("KUBERNETES_SERVICE_PORT") != "" {
+		// creates the in-cluster config
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
 	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+		// creates the out-of-cluster config
+		var kubeconfig *string
+		if home := homeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		} else {
+			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		}
+		flag.Parse()
 
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		// use the current context in kubeconfig
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
 
-	if err != nil {
-		panic(err.Error())
 	}
+
 	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
